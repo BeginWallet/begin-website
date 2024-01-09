a35f3f1af9b43e708ca07392a0bbda56b4775183
Francis Fernandes da Luz <francisluz@Franciss-MacBook-Pro-2.local>
2024-01-09 17:19:23 +0000

    graphql api updates

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -19,7 +19,7 @@ const typeDefs = /* GraphQL */ gql`
   type Query {
     apps: [App!]!,
     pools(filterPool: String, skip: Int, take: Int): [Pool!]!,
-    collections(filterPolicy: String[]): [Collection!]!, 
+    collections(filterPolicy: [String]): [Collection!]!, 
   }
 
   type App {
