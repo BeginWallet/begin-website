e2b53febf04cabf47875686df92a391c4cc1a850
Francis Fernandes da Luz <francisluz@Franciss-MacBook-Pro-2.local>
2024-02-08 00:25:35 +0000

    Remove npm packages

--- src/pages/api/countries_timezone.ts
+++ src/pages/api/countries_timezone.ts
@@ -1,60 +1,60 @@
-// import type { NextApiRequest, NextApiResponse } from 'next'
-// import { cors, runMiddleware } from './cors'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { cors, runMiddleware } from './cors'
 
-// const { countries, zones} = require('moment-timezone/data/meta/latest.json')
+const { countries, zones} = require('moment-timezone/data/meta/latest.json')
 
-// export default async function handler(
-//     // req: NextRequest
-//     req: NextApiRequest,
-//     res: NextApiResponse
-//   ) {
-//     // Run the middleware
+export default async function handler(
+    // req: NextRequest
+    req: NextApiRequest,
+    res: NextApiResponse
+  ) {
+    // Run the middleware
     
-//     let timeZoneCityToCountry = {};
+    let timeZoneCityToCountry = {};
 
-//     Object.keys(zones).forEach(z => {
-//         const cityArr = z.split("/");
-//         const city = cityArr[cityArr.length-1];
-//         timeZoneCityToCountry[city] = {
-//             name: countries[zones[z].countries[0]].name,
-//             code: countries[zones[z].countries[0]].abbr
-//         };
-//     });
+    Object.keys(zones).forEach(z => {
+        const cityArr = z.split("/");
+        const city = cityArr[cityArr.length-1];
+        timeZoneCityToCountry[city] = {
+            name: countries[zones[z].countries[0]].name,
+            code: countries[zones[z].countries[0]].abbr
+        };
+    });
 
-//     timeZoneCityToCountry = {
-//         ...timeZoneCityToCountry,
-//         "Calcutta": { "name": "India", "code": "IN" },
-//     }
+    timeZoneCityToCountry = {
+        ...timeZoneCityToCountry,
+        "Calcutta": { "name": "India", "code": "IN" },
+    }
 
-//     var userRegion;
-//     var userCity;
-//     var userCountry;
-//     var userTimeZone;
+    var userRegion;
+    var userCity;
+    var userCountry;
+    var userTimeZone;
 
-//     if (Intl) {
-//     userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-//     var tzArr = userTimeZone.split("/");
-//     userRegion = tzArr[0];
-//     userCity = tzArr[tzArr.length - 1];
-//     userCountry = timeZoneCityToCountry[userCity];
-//     }
+    if (Intl) {
+    userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    var tzArr = userTimeZone.split("/");
+    userRegion = tzArr[0];
+    userCity = tzArr[tzArr.length - 1];
+    userCountry = timeZoneCityToCountry[userCity];
+    }
 
-//     console.log("Time Zone:", userTimeZone);
-//     console.log("Region:", userRegion);
-//     console.log("City:", userCity);
-//     console.log("Country:", userCountry);
+    console.log("Time Zone:", userTimeZone);
+    console.log("Region:", userRegion);
+    console.log("City:", userCity);
+    console.log("Country:", userCountry);
 
   
-//     try {
-//       switch (req.method) {
-//         case "GET": {
-//             res.json(timeZoneCityToCountry)
-//           return 
-//         }
-//         default:
-//           break;
-//       }
-//     } catch (error: any) {
-//       return res.status(500).json({ ...error, message: error.message });
-//     }
-//   }
+    try {
+      switch (req.method) {
+        case "GET": {
+            res.json(timeZoneCityToCountry)
+          return 
+        }
+        default:
+          break;
+      }
+    } catch (error: any) {
+      return res.status(500).json({ ...error, message: error.message });
+    }
+  }
--- src/pages/api/load_collections.ts
+++ src/pages/api/load_collections.ts
@@ -1,6 +1,6 @@
-// import type { NextApiRequest, NextApiResponse } from 'next'
-// import { cors, runMiddleware } from './cors'
-// // import puppeteer from 'puppeteer'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { cors, runMiddleware } from './cors'
+// import puppeteer from 'puppeteer'
 // import puppeteer from 'puppeteer-extra';
 // import { Page } from 'puppeteer';
 // const StealthPlugin = require('puppeteer-extra-plugin-stealth');
@@ -37,99 +37,99 @@
 //     return items;
 // }
 
-// export default async function handler(
-//     // req: NextRequest
-//     req: NextApiRequest,
-//     res: NextApiResponse
-//   ) {
-//     // Run the middleware
-//     // await runMiddleware(req, res, cors)
-//     puppeteer.use(StealthPlugin())
+export default async function handler(
+    // req: NextRequest
+    req: NextApiRequest,
+    res: NextApiResponse
+  ) {
+    // Run the middleware
+    // await runMiddleware(req, res, cors)
+    // puppeteer.use(StealthPlugin())
   
-//     try {
-//       switch (req.method) {
-//         case "GET": {
-//             const baseUrl = 'https://www.jpg.store';
-//             // const url = `${baseUrl}/marketplace?view=allCollections&sortBy=score`;
-//             // const browser = await puppeteer.launch({ headless: true, timeout: 0});
-//             // const page = await browser.newPage();
+    // try {
+    //   switch (req.method) {
+    //     case "GET": {
+    //         const baseUrl = 'https://www.jpg.store';
+    //         // const url = `${baseUrl}/marketplace?view=allCollections&sortBy=score`;
+    //         // const browser = await puppeteer.launch({ headless: true, timeout: 0});
+    //         // const page = await browser.newPage();
 
-//             // //Get Collections data
-//             // // 9026afe86a4db5284395bec3d87d0b71f8b71495209af751b3763e4b
-//             // // 12a7faac878f22f0f5b32bd8e116ba6c5f23ba9cba37fe27179dec59
-//             // // dac355946b4317530d9ec0cb142c63a4b624610786c2a32137d78e25
-//             // const collectionUrl = `${baseUrl}/collection/dac355946b4317530d9ec0cb142c63a4b624610786c2a32137d78e25`
-//             // await page.goto(collectionUrl, {timeout: 0, waitUntil: 'load'})
-//             // await page.waitForSelector('script[id=__NEXT_DATA__]');
-//             // const collection = await page.evaluate(()=> {
-//             //     const name = document.querySelector('#app > main > div > section > div > div > div > div > h1')?.textContent?.trim();
-//             //     const floorRaw = document.querySelector('#app > main > div > section > div > div > div > div > div:nth-child(1) > span')?.textContent?.trim()
-//             //     const floor = (Number((floorRaw?.toLowerCase()
-//             //         .replace('-', '0')
-//             //         .replace('₳', '')
-//             //         .replace('k', '')
-//             //         .replace('m', '')
-//             //         .trim()) || 0 ) * 1000000).toString()
-//             //     const meta = JSON.parse(document.getElementById('__NEXT_DATA__')?.textContent || '')
-//             //     let policy_id = ''
-//             //     if (meta && meta.props) {
-//             //         policy_id = meta.props.pageProps.collection.policy_id
-//             //     }
+    //         // //Get Collections data
+    //         // // 9026afe86a4db5284395bec3d87d0b71f8b71495209af751b3763e4b
+    //         // // 12a7faac878f22f0f5b32bd8e116ba6c5f23ba9cba37fe27179dec59
+    //         // // dac355946b4317530d9ec0cb142c63a4b624610786c2a32137d78e25
+    //         // const collectionUrl = `${baseUrl}/collection/dac355946b4317530d9ec0cb142c63a4b624610786c2a32137d78e25`
+    //         // await page.goto(collectionUrl, {timeout: 0, waitUntil: 'load'})
+    //         // await page.waitForSelector('script[id=__NEXT_DATA__]');
+    //         // const collection = await page.evaluate(()=> {
+    //         //     const name = document.querySelector('#app > main > div > section > div > div > div > div > h1')?.textContent?.trim();
+    //         //     const floorRaw = document.querySelector('#app > main > div > section > div > div > div > div > div:nth-child(1) > span')?.textContent?.trim()
+    //         //     const floor = (Number((floorRaw?.toLowerCase()
+    //         //         .replace('-', '0')
+    //         //         .replace('₳', '')
+    //         //         .replace('k', '')
+    //         //         .replace('m', '')
+    //         //         .trim()) || 0 ) * 1000000).toString()
+    //         //     const meta = JSON.parse(document.getElementById('__NEXT_DATA__')?.textContent || '')
+    //         //     let policy_id = ''
+    //         //     if (meta && meta.props) {
+    //         //         policy_id = meta.props.pageProps.collection.policy_id
+    //         //     }
 
-//             //     return {
-//             //         name,
-//             //         floor,
-//             //         policy_id,
-//             //     }
-//             // })
+    //         //     return {
+    //         //         name,
+    //         //         floor,
+    //         //         policy_id,
+    //         //     }
+    //         // })
 
-//             // Fetch
-//             const collectionUrl = `${baseUrl}/collection/12a7faac878f22f0f5b32bd8e116ba6c5f23ba9cba37fe27179dec59`
-//             const result = await fetch(collectionUrl);
-//             const html = await result.text();
+    //         // Fetch
+    //         const collectionUrl = `${baseUrl}/collection/12a7faac878f22f0f5b32bd8e116ba6c5f23ba9cba37fe27179dec59`
+    //         const result = await fetch(collectionUrl);
+    //         const html = await result.text();
 
-//             const dom = new JSDOM(html);
-//             const document = dom.window.document;
-//             const collection = () => {
-//                 const meta = JSON.parse(document.getElementById('__NEXT_DATA__')?.textContent || '')
-//                 let policy_id = ''
-//                 let name = ''
-//                 let floor = ''
-//                 if (meta && meta.props) {
-//                     policy_id = meta.props.pageProps.collection.policy_id;
-//                     name = meta.props.pageProps.collection.display_name;
-//                     floor = meta.props.pageProps.collection.global_floor_lovelace;
-//                 }
+    //         const dom = new JSDOM(html);
+    //         const document = dom.window.document;
+    //         const collection = () => {
+    //             const meta = JSON.parse(document.getElementById('__NEXT_DATA__')?.textContent || '')
+    //             let policy_id = ''
+    //             let name = ''
+    //             let floor = ''
+    //             if (meta && meta.props) {
+    //                 policy_id = meta.props.pageProps.collection.policy_id;
+    //                 name = meta.props.pageProps.collection.display_name;
+    //                 floor = meta.props.pageProps.collection.global_floor_lovelace;
+    //             }
 
-//                 return {
-//                     policy_id,
-//                     name,
-//                     floor,
-//                 }
-//             }
+    //             return {
+    //                 policy_id,
+    //                 name,
+    //                 floor,
+    //             }
+    //         }
 
-//             console.log(collection())
+    //         console.log(collection())
 
-//             // await page.goto(url, { timeout: 0, waitUntil: 'load'});
-//             // // await new Promise(r => setTimeout(r, 5000));
-//             // const items = await infiniteScrollItems(baseUrl, page, 5000);
-//             // fs.writeFileSync("collections.json", JSON.stringify(items))
+    //         // await page.goto(url, { timeout: 0, waitUntil: 'load'});
+    //         // // await new Promise(r => setTimeout(r, 5000));
+    //         // const items = await infiniteScrollItems(baseUrl, page, 5000);
+    //         // fs.writeFileSync("collections.json", JSON.stringify(items))
             
-//             // await page.screenshot({ path: 'jpg.jpg', fullPage: true});
+    //         // await page.screenshot({ path: 'jpg.jpg', fullPage: true});
 
-//             // await browser.close();
+    //         // await browser.close();
 
-//             res.json({
-//                 load: 'done',
-//                 collection: collection()
-//                 // len: items.length
-//               })
-//           return 
-//         }
-//         default:
-//           break;
-//       }
-//     } catch (error: any) {
-//       return res.status(500).json({ ...error, message: error.message });
-//     }
-//   }
+    //         res.json({
+    //             load: 'done',
+    //             collection: collection()
+    //             // len: items.length
+    //           })
+    //       return 
+    //     }
+    //     default:
+    //       break;
+    //   }
+    // } catch (error: any) {
+    //   return res.status(500).json({ ...error, message: error.message });
+    // }
+  }
